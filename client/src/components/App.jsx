c6cddbdfaa499380fe219e476cb7c9eae7128d8f
Dennis Kim <dekim2324@gmail.com>
2019-10-11 12:08:38 -0700

    Used styled-component to set up backbone

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Section1 from './section1/Section1.jsx';
 import Section2 from './section2/Section2.jsx';
 import Section3 from './section3/Section3.jsx';
@@ -13,15 +14,39 @@ class App extends Component {
 
         return (
             <div>
-                <Section1 />
-                <Section2 />
-                <Section3 />
+                <Summary>
+                summary top section
+                </Summary>
+                <Spec>
+                    spec
+                </Spec>
+                <Rundown>
+                    rundown
+                </Rundown>
             </div>
         )
     }
 }
 
+const Summary = styled.div`
+    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
+    font-size: 14px;
+    line-height: 1.43;
+    color: #484848;
+    box-sizing: border-box;
+    width: 594px;
+    height: 141px;
+    background-color: grey;
+`; 
 
+const Spec = styled(Summary)`
+    height: 210.955px;
+    background-color: brown;
+`;
 
+const Rundown = styled(Summary)`
+    height: 287.604px;
+    background-color: purple;
+`;
 
 export default App;
